12b3a7f456c2e0ad6dc6ea83f6a1a0cc334073ac
Nqobile <148335925+n-qobile@users.noreply.github.com>
2025-12-09 09:07:06 +0200

    Fix quotes and formatting in degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,24 +1,24 @@
 const degrees = [
-{
-school: ‘Project Y’,
-degree: ‘FullStack Development Program’,
-link: ‘https://projecty.co.za’,
-year: 2025,
-},
+  {
+    school: 'Project Y',
+    degree: 'FullStack Development Program',
+    link: 'https://projecty.co.za',
+    year: 2025,
+  },
   
-{
-school: ‘SheCodes, Codecademy, Coursera’,
-degree: ‘Coding Certifications & Courses’,
-link: ‘/stats’,
-year: 2024,
-},
+  {
+    school: 'SheCodes, Codecademy, Coursera',
+    degree: 'Coding Certifications & Courses',
+    link: '/stats',
+    year: 2025,
+  },
   
-{
-school: ‘University of Cape Town’,
-degree: ‘B.Sc. Speech-Language Pathology’,
-link: ‘https://www.uct.ac.za/’,
-year: 2021,
-},
+  {
+    school: 'University of Cape Town',
+    degree: 'B.Sc. Speech-Language Pathology',
+    link: 'https://www.uct.ac.za/',
+    year: 2021,
+  },
 ];
 
 export default degrees;
