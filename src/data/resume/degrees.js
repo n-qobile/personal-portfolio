b39a50bc8466c659bf85a504913f109c29ad9ce2
Nqobile <148335925+n-qobile@users.noreply.github.com>
2025-12-09 08:36:31 +0200

    Update degrees list with new entries and formatting

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,16 +1,22 @@
 const degrees = [
-  {
-    school: 'SheCodes, Codecademy',
-    degree: 'Coding certifications',
-    link: '/stats',
-    year: 2024,
-  },
-  {
-    school: 'University of Cape Town',
-    degree: 'B.Sc. Speech-Language Pathology',
-    link: 'https://www.uct.ac.za/',
-    year: 2021,
-  },
+{
+school: ‘Project Y’,
+degree: ‘Full-Stack Development Program’,
+link: ‘https://projecty.co.za’,
+year: 2025,
+},
+{
+school: ‘SheCodes, Codecademy, Coursera’,
+degree: ‘Coding Certifications & Courses’,
+link: ‘/stats’,
+year: 2024,
+},
+{
+school: ‘University of Cape Town’,
+degree: ‘B.Sc. Speech-Language Pathology’,
+link: ‘https://www.uct.ac.za/’,
+year: 2021,
+},
 ];
 
 export default degrees;
